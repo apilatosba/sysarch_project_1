7b178c3c935663798187b0c5fed5665785706055
apilatosba <aposdapulos098@outlook.com>
2025-05-31 20:20:10 +0200

    idk what i do

--- src/main/scala/bitmanipulation/Shuffler.scala
+++ src/main/scala/bitmanipulation/Shuffler.scala
@@ -12,8 +12,70 @@ abstract class AbstractShuffler(bitWidth: Int) extends Module {
   })
 }
 
+class ShufflerSubCircuit(bitWidth: Int, shiftAmount: Int) extends Module {
+    val io = IO(new Bundle {
+    val input  = Input(UInt(bitWidth.W))
+    val enable = Input(Bool())
+    val output = Output(UInt(bitWidth.W))
+  })
+
+  def GenerateLeftMask(bitWidth: Int, shiftAmount: Int): UInt = {
+    var result = BigInt(0)
+    for (j <- 0 until bitWidth) {
+      if (((j / shiftAmount) & 1) == 0) {
+        result = result.setBit(j)
+      }
+    }
+    return result.U(bitWidth.W)
+  }
+
+  val maskL = GenerateLeftMask(bitWidth, shiftAmount)
+  val maskR = (~maskL).asUInt
+
+  when (io.enable) {
+    val leftPortion  = (io.input & maskL) << shiftAmount
+    val rightPortion = (io.input & maskR) >> shiftAmount
+    io.output := leftPortion | rightPortion
+  } .otherwise {
+    io.output := io.input
+  }
+}
+
 class Shuffler(bitWidth: Int) extends AbstractShuffler(bitWidth) {
 
-  ??? // TODO: implement Task 1.3 here
+  // ??? // TODO: implement Task 1.3 here
 
+  val stages = log2Ceil(bitWidth) - 1
+
+  val shflResult = {
+    var current: UInt = io.input
+
+    for (i <- (stages - 1) to 0 by -1) {
+      val shiftAmt = 1 << i
+      val stageSub = Module(new ShufflerSubCircuit(bitWidth, shiftAmt))
+      stageSub.io.enable := io.pattern(i)
+      stageSub.io.input := current
+      current = stageSub.io.output
+    }
+    current
+  }
+
+  val ushflResult = {
+    var current: UInt = io.input
+
+    for (i <- 0 until stages) {
+      val shiftAmt = 1 << i
+      val stageSub = Module(new ShufflerSubCircuit(bitWidth, shiftAmt))
+      stageSub.io.enable := io.pattern(i)
+      stageSub.io.input := current
+      current = stageSub.io.output
+    }
+    current
+  }
+
+  when (io.unshuffle.asBool) {
+    io.result := ushflResult
+  } .otherwise {
+    io.result := shflResult
+  }
 }
