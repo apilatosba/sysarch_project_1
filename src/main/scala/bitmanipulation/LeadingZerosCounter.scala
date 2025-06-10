511adecf97e4383347036fc970cb6a01ff952a29
apilatosba <aposdapulos098@outlook.com>
2025-06-10 11:19:06 +0200

    leadingzeros

--- src/main/scala/bitmanipulation/LeadingZerosCounter.scala
+++ src/main/scala/bitmanipulation/LeadingZerosCounter.scala
@@ -24,6 +24,8 @@ class LeadingZerosCounter(bitWidth: Int)
   // and the base is just when n is 1 and the result is just the negation of the input
   //
   // actually above thing is wrong now i noticed that the input is not in the form 2^n but is in the form of 2*n
+  //
+  // above thing is correct input is a power of two
 
   if (bitWidth == 1) {
     io.result := ~io.input
@@ -33,21 +35,47 @@ class LeadingZerosCounter(bitWidth: Int)
     val upperHalf = io.input(bitWidth - 1, halfWidth)
     val lowerHalf = io.input(halfWidth - 1, 0)
 
+
     val upperSubCircuit = Module(new LeadingZerosCounter(halfWidth))
     upperSubCircuit.io.input := upperHalf
 
     val lowerSubCircuit = Module(new LeadingZerosCounter(halfWidth))
     lowerSubCircuit.io.input := lowerHalf
 
-    when (upperSubCircuit.io.result === halfWidth.U) {
-      // when (lowerSubCircuit.io.result(resultBitWidth - 2) === 1.U) {
-      //   io.result := Cat(1.B, Fill(resultBitWidth - 1, 0.B))
-      // } .otherwise {
-      //   io.result := Cat(0.B, upperSubCircuit.io.result | lowerSubCircuit.io.result)
-      // }
-      io.result := upperSubCircuit.io.result +& lowerSubCircuit.io.result
+    when (upperSubCircuit.io.result(resultBitWidth - 2) === 1.U) {
+      when (lowerSubCircuit.io.result(resultBitWidth - 2) === 1.U) {
+        io.result := 1.U(1.W) ## Fill(resultBitWidth - 1, 0.U)
+      } .otherwise {
+        io.result := upperSubCircuit.io.result | lowerSubCircuit.io.result
+      }
     } .otherwise {
       io.result := upperSubCircuit.io.result
     }
   }
+
+  // if (bitWidth == 1) {
+  //   io.result := ~io.input
+  // } else {
+  //   val resultBitWidth = log2Ceil(bitWidth + 1)
+  //   val halfWidth = bitWidth / 2
+  //   val upperHalf = io.input(bitWidth - 1, halfWidth)
+  //   val lowerHalf = io.input(halfWidth - 1, 0)
+
+  //   val upperSubCircuit = Module(new LeadingZerosCounter(halfWidth))
+  //   upperSubCircuit.io.input := upperHalf
+
+  //   val lowerSubCircuit = Module(new LeadingZerosCounter(halfWidth))
+  //   lowerSubCircuit.io.input := lowerHalf
+
+  //   when (upperSubCircuit.io.result === halfWidth.U) {
+  //     // when (lowerSubCircuit.io.result(resultBitWidth - 2) === 1.U) {
+  //     //   io.result := Cat(1.B, Fill(resultBitWidth - 1, 0.B))
+  //     // } .otherwise {
+  //     //   io.result := Cat(0.B, upperSubCircuit.io.result | lowerSubCircuit.io.result)
+  //     // }
+  //     io.result := upperSubCircuit.io.result +& lowerSubCircuit.io.result
+  //   } .otherwise {
+  //     io.result := upperSubCircuit.io.result
+  //   }
+  // }
 }
