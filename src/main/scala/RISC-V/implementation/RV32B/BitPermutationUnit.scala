e5676ab4c15d00c6b20d71420f6dd17df96d7bce
apilatosba <aposdapulos098@outlook.com>
2025-06-11 21:49:49 +0200

    shuffler

--- src/main/scala/RISC-V/implementation/RV32B/BitPermutationUnit.scala
+++ src/main/scala/RISC-V/implementation/RV32B/BitPermutationUnit.scala
@@ -8,6 +8,8 @@ import RISCV.model._
 import bitmanipulation.AbstractGeneralizedReverser
 import bitmanipulation.AbstractShuffler
 import bitmanipulation.AbstractSequentialRotater
+import RISCV.implementation.RV32I.ControlUnit
+import RISCV.implementation.RV32I.Decoder
 
 class BitPermutationUnit(
     genGeneralizedReverser: () => AbstractGeneralizedReverser,
@@ -17,22 +19,155 @@ class BitPermutationUnit(
 
   io.misa := "b01__0000__0_00000_00000_00000_00000_00010".U
 
-  val generalizedReverser = Module(genGeneralizedReverser())
-  val shuffler = Module(genShuffler())
-  val rotater = Module(genRotater())
+  // val generalizedReverser = Module(genGeneralizedReverser())
+  // val shuffler = Module(genShuffler())
+  // val rotater = Module(genRotater())
 
-  io.stall := STALL_REASON.NO_STALL
+  // io.stall := STALL_REASON.NO_STALL
 
+  // io_data <> DontCare
+  // io_reg <> DontCare
+  // io_pc <> DontCare
+  // io_reset <> DontCare
+  // io_trap <> DontCare
+
+  // generalizedReverser.io <> DontCare
+  // shuffler.io <> DontCare
+  // rotater.io <> DontCare
+
+  // ??? // TODO: implement Task 2.5 here
+
+
+
+
+
+
+
+  // decode & (re‐)use the RV32I control for stall=NO_STALL by default
+  val control = Module(new ControlUnit)
+  control.io_reset    <> io_reset
+  control.io_ctrl.instr_type := instr_type
+  control.io_ctrl.data_gnt    := true.B
+  io.stall := control.io_ctrl.stall
+
+  // decoder to extract rs1,rs2,rd,imm
+  val dec = Module(new Decoder)
+  dec.io_reset         <> io_reset
+  dec.io_decoder.instr := io.instr
+
+  // submodules
+  val grev   = Module(genGeneralizedReverser())
+  val shfl   = Module(genShuffler())
+  val rot    = Module(genRotater())
+
+  // default: pacify unused
   io_data <> DontCare
-  io_reg <> DontCare
-  io_pc <> DontCare
-  io_reset <> DontCare
+  io_pc   <> DontCare
   io_trap <> DontCare
 
-  generalizedReverser.io <> DontCare
-  shuffler.io <> DontCare
-  rotater.io <> DontCare
+  grev.io    <> DontCare
+  shfl.io    <> DontCare
+  rot.io     <> DontCare
 
-  ??? // TODO: implement Task 2.5 here
+  // PC always +4
+  io_pc.pc_wdata := io_pc.pc + 4.U
+  io_pc.pc_we    := control.io_ctrl.stall === STALL_REASON.NO_STALL
+
+  // register‐file address ports from decoder
+  io_reg.reg_rs1 := dec.io_decoder.rs1
+  io_reg.reg_rs2 := dec.io_decoder.rs2
+  io_reg.reg_rd  := dec.io_decoder.rd
+
+  // default writeback off & data = 0
+  io_reg.reg_write_en   := false.B
+  io_reg.reg_write_data := 0.U
+
+  // extract operands & immediate/pattern
+  val a        = io_reg.reg_read_data1
+  val b        = io_reg.reg_read_data2
+  val imm5     = dec.io_decoder.instr(24,20)
+  val patt5    = b(4,0)       // dynamic pattern from rs2 low bits
+  val reverseA = Reverse(a)
+
+  switch(instr_type) {
+    // ---- Combinational GREV ----
+    is(RISCV_TYPE.grev) {
+      grev.io.input   := a
+      grev.io.pattern := patt5
+      io_reg.reg_write_en   := true.B
+      io_reg.reg_write_data := grev.io.result
+    }
+
+    // ---- Combinational SHFL / UNSHFL ----
+    is(RISCV_TYPE.shfl) {
+      shfl.io.input     := a
+      shfl.io.pattern   := patt5
+      shfl.io.unshuffle := 0.U
+      io_reg.reg_write_en   := true.B
+      io_reg.reg_write_data := shfl.io.result
+    }
+    is(RISCV_TYPE.unshfl) {
+      shfl.io.input     := a
+      shfl.io.pattern   := patt5
+      shfl.io.unshuffle := 1.U
+      io_reg.reg_write_en   := true.B
+      io_reg.reg_write_data := shfl.io.result
+    }
+
+    // ---- Immediate GREV I / SHFL I / UNSHFL I ----
+    is(RISCV_TYPE.grevi) {
+      grev.io.input   := a
+      grev.io.pattern := imm5
+      io_reg.reg_write_en   := true.B
+      io_reg.reg_write_data := grev.io.result
+    }
+    is(RISCV_TYPE.shfli) {
+      shfl.io.input     := a
+      shfl.io.pattern   := imm5
+      shfl.io.unshuffle := 0.U
+      io_reg.reg_write_en   := true.B
+      io_reg.reg_write_data := shfl.io.result
+    }
+    is(RISCV_TYPE.unshfli) {
+      shfl.io.input     := a
+      shfl.io.pattern   := imm5
+      shfl.io.unshuffle := 1.U
+      io_reg.reg_write_en   := true.B
+      io_reg.reg_write_data := shfl.io.result
+    }
+
+    // ---- Sequential ROTATES: ROL / ROR / RORI ----
+    is(RISCV_TYPE.rol) {
+      rot.io.input := a
+      rot.io.shamt := patt5
+      rot.io.start := true.B
+      io.stall     := STALL_REASON.EXECUTION_UNIT
+    }
+    is(RISCV_TYPE.ror) {
+      rot.io.input := a
+      rot.io.shamt := patt5
+      rot.io.start := true.B
+      io.stall     := STALL_REASON.EXECUTION_UNIT
+    }
+    is(RISCV_TYPE.rori) {
+      rot.io.input := a
+      rot.io.shamt := imm5
+      rot.io.start := true.B
+      io.stall     := STALL_REASON.EXECUTION_UNIT
+    }
+  }
+
+  // feed sequential rotater and write back when done
+  // these two lines apply regardless of which rotate variant
+  when(instr_type.isOneOf(RISCV_TYPE.rol, RISCV_TYPE.ror, RISCV_TYPE.rori)) {
+    // keep driving inputs
+    // rot.io.input and shamt already set above
+    rot.io.start := false.B     // only true in the cycle we matched above
+    io.stall     := Mux(rot.io.done, STALL_REASON.NO_STALL, STALL_REASON.EXECUTION_UNIT)
+    when(rot.io.done) {
+      io_reg.reg_write_en   := true.B
+      io_reg.reg_write_data := rot.io.result
+    }
+  }
 
 }
--- src/main/scala/bitmanipulation/Shuffler.scala
+++ src/main/scala/bitmanipulation/Shuffler.scala
@@ -12,69 +12,98 @@ abstract class AbstractShuffler(bitWidth: Int) extends Module {
   })
 }
 
-class ShufflerSubCircuit(bitWidth: Int, shiftAmount: Int) extends Module {
-    val io = IO(new Bundle {
-    val input  = Input(UInt(bitWidth.W))
-    val enable = Input(Bool())
-    val output = Output(UInt(bitWidth.W))
-  })
-
-  def GenerateLeftMask(bitWidth: Int, shiftAmount: Int): UInt = {
-    var result = BigInt(0)
-    for (j <- 0 until bitWidth) {
-      if (((j / shiftAmount) & 1) == 0) {
-        result = result.setBit(j)
-      }
-    }
-    return result.U(bitWidth.W)
-  }
-
-  val maskL = GenerateLeftMask(bitWidth, shiftAmount)
-  val maskR = (~maskL).asUInt
-
-  when (io.enable) {
-    val leftPortion  = (io.input & maskL) << shiftAmount
-    val rightPortion = (io.input & maskR) >> shiftAmount
-    io.output := leftPortion | rightPortion
-  } .otherwise {
-    io.output := io.input
-  }
-}
-
 class Shuffler(bitWidth: Int) extends AbstractShuffler(bitWidth) {
 
   // ??? // TODO: implement Task 1.3 here
 
-  val stages = log2Ceil(bitWidth) - 1
+  // uint32_t shuffle32_stage(uint32_t src, uint32_t maskL, uint32_t maskR, int N)
+  // {
+  //    uint32_t x = src & ~(maskL | maskR);
+  //    x |= ((src << N) & maskL) | ((src >> N) & maskR);
+  //    return x;
+  // }
 
-  val shflResult = {
-    var current: UInt = io.input
+  // uint32_t shfl32(uint32_t rs1, uint32_t rs2)
+  // {
+  //    uint32_t x = rs1;
+  //    int shamt = rs2 & 15;
+  //    if (shamt & 8) x = shuffle32_stage(x, 0x00ff0000, 0x0000ff00, 8);
+  //    if (shamt & 4) x = shuffle32_stage(x, 0x0f000f00, 0x00f000f0, 4);
+  //    if (shamt & 2) x = shuffle32_stage(x, 0x30303030, 0x0c0c0c0c, 2);
+  //    if (shamt & 1) x = shuffle32_stage(x, 0x44444444, 0x22222222, 1);
+  //    return x;
+  // }
 
-    for (i <- (stages - 1) to 0 by -1) {
-      val shiftAmt = 1 << i
-      val stageSub = Module(new ShufflerSubCircuit(bitWidth, shiftAmt))
-      stageSub.io.enable := io.pattern(i)
-      stageSub.io.input := current
-      current = stageSub.io.output
-    }
-    current
+  // uint32_t unshfl32(uint32_t rs1, uint32_t rs2)
+  // {
+  //    uint32_t x = rs1;
+  //    int shamt = rs2 & 15;
+  //    if (shamt & 1) x = shuffle32_stage(x, 0x44444444, 0x22222222, 1);
+  //    if (shamt & 2) x = shuffle32_stage(x, 0x30303030, 0x0c0c0c0c, 2);
+  //    if (shamt & 4) x = shuffle32_stage(x, 0x0f000f00, 0x00f000f0, 4);
+  //    if (shamt & 8) x = shuffle32_stage(x, 0x00ff0000, 0x0000ff00, 8);
+  //    return x;
+  // }
+
+  def ShuffleStage(src: UInt, N: Int, maskL: UInt, maskR: UInt): UInt = {
+    val x = src & ~(maskL | maskR)
+    val shiftedBits = ((src << N) & maskL) | ((src >> N) & maskR)
+    x | shiftedBits
   }
 
-  val ushflResult = {
-    var current: UInt = io.input
+  def GenerateStageMasks(bitWidth: Int): Seq[(Int, BigInt, BigInt)] = {
+    val numStages = log2Ceil(bitWidth) - 1
+    (0 until numStages).map { i =>
+      val N = 1 << i
+      val groupSize = 4 * N
+      var maskL = BigInt(0)
+      var maskR = BigInt(0)
 
-    for (i <- 0 until stages) {
-      val shiftAmt = 1 << i
-      val stageSub = Module(new ShufflerSubCircuit(bitWidth, shiftAmt))
-      stageSub.io.enable := io.pattern(i)
-      stageSub.io.input := current
-      current = stageSub.io.output
+      for (j <- 0 until bitWidth) {
+        val j_in_group = j % groupSize
+        if (j_in_group >= N && j_in_group < 2 * N) {
+          maskR = maskR.setBit(j)
+        }
+        if (j_in_group >= 2 * N && j_in_group < 3 * N) {
+          maskL = maskL.setBit(j)
+        }
+      }
+      (N, maskL, maskR)
     }
-    current
   }
 
-  when (io.unshuffle.asBool) {
-    io.result := ushflResult
+  val stageMasks = GenerateStageMasks(bitWidth)
+
+  var currentShfl = io.input
+  for (i <- (stageMasks.length - 1) to 0 by -1) {
+    val (n, maskL, maskR) = stageMasks(i)
+    val nextShfl = Wire(UInt(bitWidth.W))
+
+    when(io.pattern(i)) {
+      nextShfl := ShuffleStage(currentShfl, n, maskL.U(bitWidth.W), maskR.U(bitWidth.W))
+    }.otherwise {
+      nextShfl := currentShfl
+    }
+    currentShfl = nextShfl
+  }
+  val shflResult = currentShfl
+
+  var currentUnshfl = io.input
+  for (i <- 0 until stageMasks.length) {
+    val (n, maskL, maskR) = stageMasks(i)
+    val nextUnshfl = Wire(UInt(bitWidth.W))
+
+    when(io.pattern(i)) {
+      nextUnshfl := ShuffleStage(currentUnshfl, n, maskL.U(bitWidth.W), maskR.U(bitWidth.W))
+    }.otherwise {
+      nextUnshfl := currentUnshfl
+    }
+    currentUnshfl = nextUnshfl
+  }
+  val unshflResult = currentUnshfl
+
+  when(io.unshuffle.asBool) {
+    io.result := unshflResult
   } .otherwise {
     io.result := shflResult
   }
